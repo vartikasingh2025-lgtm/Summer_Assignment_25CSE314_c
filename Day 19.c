df9e69f9c929d2838934bcf95f3a8a8dd9999d22
unknown <vartika.singh2025@glbajajgroup.org>
2026-06-27 21:01:29 +0530

    Day 18.c uploaded

---
+++ Day 19.c
@@ -0,0 +1,139 @@
+/*Add matrices*/
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], c[10][10];
+    int r, col, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &col);
+
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("Sum of matrices:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+/*Subtract matrices*/
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], c[10][10];
+    int r, col, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &col);
+
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+
+    printf("Difference of matrices:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+/*Transpose matrix*/
+
+#include <stdio.h>
+
+int main() {
+    int a[10][10], t[10][10];
+    int r, col, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &col);
+
+    printf("Enter elements of matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < col; j++) {
+            t[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose of matrix:\n");
+    for (i = 0; i < col; i++) {
+        for (j = 0; j < r; j++) {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+/*Diagonal sum*/
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int n, i, j, sum = 0;
+
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter elements of matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        sum = sum + a[i][i];
+    }
+
+    printf("Sum of diagonal elements = %d\n", sum);
+
+    return 0;
+}
