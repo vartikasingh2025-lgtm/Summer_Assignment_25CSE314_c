faa76233abf2df10c2210f2118abaee85fda3693
unknown <vartika.singh2025@glbajajgroup.org>
2026-07-02 20:33:38 +0530

    Day 21.c uploaded

---
+++ Day 21.c
@@ -0,0 +1,91 @@
+/*string length without using strlen() */
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    printf("Length of string = %d\n", length - 1); // Excluding '\n'
+    return 0;
+}
+/*Reverse a string*/
+#include <stdio.h>
+
+int main() {
+    char str[100], temp;
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    for (i = 0; i < length / 2; i++) {
+        temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
+/*count vowels and consonants*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        char ch = str[i];
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+                ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+        i++;
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d\n", consonants);
+
+    return 0;
+}
+/*convert lowercase to uppercase*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+
+    printf("Uppercase string: %s", str);
+
+    return 0;
+}
